4fbc4211335682284dbba1906f6e591a707e3353
Jeffrey04 <mycyberpet@yahoo.com>
2018-12-04 20:29:13 +0800

    force crawler to update view after every round

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -20,12 +20,12 @@ conn = pymysql.connect(
     password=os.environ.get('DB_PASS', 'abc123'),
     db=os.environ.get('DB_NAME', 'censorship'),
     charset='utf8mb4',
-    autocommit=True,
     cursorclass=pymysql.cursors.DictCursor)
 
 with conn:
     with conn.cursor() as _fetch_cursor:
-        _fetch_cursor.execute('''
+        _fetch_cursor.execute(
+            '''
                               SELECT     *
                               FROM       sites s
                               JOIN       (
@@ -42,8 +42,10 @@ with conn:
                          os.environ.get('COUNTRY_CODE', 'MY'), row['url'])
             response = requests.get(
                 'https://api.ooni.io/api/v1/measurements',
-                params={'probe_cc': row['country_code'],
-                        'input': row['url']})
+                params={
+                    'probe_cc': row['country_code'],
+                    'input': row['url']
+                })
 
             if not response.ok:
                 logging.info(
@@ -51,10 +53,9 @@ with conn:
                     os.environ.get('COUNTRY_CODE', 'MY'), row['url'])
                 continue
 
-            result_list = response.json()
-
+            conn.begin()
             with conn.cursor() as _insert_cursor:
-                for result in result_list.get('results', []):
+                for result in response.json().get('results', []):
                     _insert_cursor.execute(
                         '''
                         REPLACE
@@ -63,10 +64,31 @@ with conn:
                         '''.format(', '.join(FIELDS), ', '.join(
                             '%s' for _ in range(len(FIELDS)))),
                         tuple(
-                            dateutil.parser.parse(result[field])
-                            if field == 'measurement_start_time' else
-                            result[field] for field in FIELDS))
+                            dateutil.parser.parse(result[field]) if field ==
+                            'measurement_start_time' else result[field]
+                            for field in FIELDS))
+
+            conn.commit()
 
             time.sleep(float(os.environ.get('SLEEP_TIME', 2)))
 
+        # write summary
+        conn.begin()
+        with conn.cursor() as _cur:
+            _cur.execute(
+                '''
+                INSERT INTO summary_view(year, country, category, count)
+                    SELECT      YEAR(measurement_start_time) AS year,
+                                m.probe_cc AS country,
+                                s.category_code AS category,
+                                COUNT(DISTINCT m.input) AS count
+                    FROM        measurements m
+                    JOIN        sites s
+                    ON          (m.input = s.url
+                                    AND m.probe_cc = s.country_code)
+                    WHERE       YEAR(measurement_start_time) = YEAR(NOW())
+                                AND m.probe_cc = %s
+                                AND (anomaly = TRUE OR confirmed = TRUE)
+                    GROUP BY    YEAR(measurement_start_time), m.probe_cc, s.category_code;
+                ''', (os.environ.get('COUNTRY_CODE', 'MY'), ))
         conn.commit()
