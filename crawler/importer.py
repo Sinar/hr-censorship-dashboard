bbb48649397bb3953fd93cd86630da20c544d7f6
Jeffrey04 <mycyberpet@yahoo.com>
2020-02-17 16:51:04 +0800

    consolidate importing scripts into one

--- crawler/importer.py
+++ crawler/importer.py
@@ -1,24 +1,26 @@
 from datetime import datetime
 import os, os.path
+import csv
 
-from odo import odo
+from odo import odo, drop
 import dask.dataframe as df
 
 
-def main():
-    _file = os.environ.get('IMPORT_FILE', 'test-lists/lists/my.csv')
+def import_test_lists():
+    _file = os.environ.get("IMPORT_FILE", "test-lists/lists/my.csv")
     frame = df.read_csv(_file)
-    frame['country_code'] = os.path.splitext(
-        os.path.basename(_file))[0].upper()
-    frame['import_date'] = datetime.now()
+    frame["country_code"] = os.path.splitext(os.path.basename(_file))[0].upper()
+    frame["import_date"] = datetime.now()
 
-    odo(frame.compute().fillna(''),
-        'mysql+pymysql://{}:{}@{}:{}/{}::sites'.format(
-            os.environ.get('DB_USER', 'root'),
-            os.environ.get('DB_PASS', 'abc123'),
-            os.environ.get('DB_HOST', 'localhost'),
-            os.environ.get('DB_PORT', '3306'),
-            os.environ.get('DB_NAME', 'censorship')),
+    odo(
+        frame.compute().fillna(""),
+        "mysql+pymysql://{}:{}@{}:{}/{}::sites".format(
+            os.environ.get("DB_USER", "root"),
+            os.environ.get("DB_PASS", "abc123"),
+            os.environ.get("DB_HOST", "localhost"),
+            os.environ.get("DB_PORT", "3306"),
+            os.environ.get("DB_NAME", "censorship"),
+        ),
         dshape="""
                var * {
                    url: string,
@@ -30,8 +32,53 @@ def main():
                    country_code: string,
                    import_date: datetime,
                }
-               """)
+               """,
+    )
 
 
-if __name__ == '__main__':
+def import_asn():
+    try:
+        drop(
+            "mysql+pymysql://{}:{}@{}:{}/{}::asn".format(
+                os.environ.get("DB_USER", "root"),
+                os.environ.get("DB_PASS", "abc123"),
+                os.environ.get("DB_HOST", "localhost"),
+                os.environ.get("DB_PORT", "3306"),
+                os.environ.get("DB_NAME", "censorship"),
+            )
+        )
+    except ValueError:
+        pass
+
+    with open(
+        os.environ.get("IMPORT_FILE", "geoip-list/GeoLite2-ASN-Blocks-IPv4.csv")
+    ) as f:
+        odo(
+            [row for row in csv.DictReader(f, skipinitialspace=True)],
+            "mysql+pymysql://{}:{}@{}:{}/{}::asn".format(
+                os.environ.get("DB_USER", "root"),
+                os.environ.get("DB_PASS", "abc123"),
+                os.environ.get("DB_HOST", "localhost"),
+                os.environ.get("DB_PORT", "3306"),
+                os.environ.get("DB_NAME", "censorship"),
+            ),
+            dshape="""
+                var * {
+                    network: string,
+                    autonomous_system_number: string,
+                    autonomous_system_organization: string
+                }
+            """,
+            local="LOCAL",
+        )
+
+
+def main():
+    if os.environ.get("IMPORT_TYPE", "TEST_LIST"):
+        import_test_lists()
+    else:
+        import_asn()
+
+
+if __name__ == "__main__":
     main()
