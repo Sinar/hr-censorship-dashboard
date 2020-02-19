2f193c828015037a2fb43937f58caa91dac43e29
Jeffrey04 <mycyberpet@yahoo.com>
2020-02-19 23:35:50 +0800

    importer write index

--- crawler/importer.py
+++ crawler/importer.py
@@ -4,9 +4,16 @@ import csv
 
 from odo import odo, drop
 import dask.dataframe as df
+import pymysql
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def import_test_lists():
+    logging.info("Importing test-lists")
     _file = os.environ.get("IMPORT_FILE", "test-lists/lists/my.csv")
     frame = df.read_csv(_file)
     frame["country_code"] = os.path.splitext(os.path.basename(_file))[0].upper()
@@ -38,6 +45,7 @@ def import_test_lists():
 
 def import_asn():
     try:
+        logging.info("Dropping old asn-lists if exists")
         drop(
             "mysql+pymysql://{}:{}@{}:{}/{}::asn".format(
                 os.environ.get("DB_USER", "root"),
@@ -53,8 +61,11 @@ def import_asn():
     with open(
         os.environ.get("IMPORT_FILE", "geoip-list/GeoLite2-ASN-Blocks-IPv4.csv")
     ) as f:
+        logging.info("Importing asn-lists")
         odo(
-            [row for row in csv.DictReader(f, skipinitialspace=True)],
+            [dict(row, autonomous_system_number=f'AS{row["autonomous_system_number"]}')
+             for row
+             in csv.DictReader(f, skipinitialspace=True)],
             "mysql+pymysql://{}:{}@{}:{}/{}::asn".format(
                 os.environ.get("DB_USER", "root"),
                 os.environ.get("DB_PASS", "abc123"),
@@ -65,19 +76,31 @@ def import_asn():
             dshape="""
                 var * {
                     network: string,
-                    autonomous_system_number: string,
+                    autonomous_system_number: string[8],
                     autonomous_system_organization: string
                 }
             """,
             local="LOCAL",
         )
 
+    logging.info("Building index for asn-lists")
+    _conn = pymysql.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASS", "abc123"),
+        db=os.environ.get("DB_NAME", "censorship"))
+    with _conn.cursor() as cur:
+        cur.execute('''
+                    CREATE INDEX asn_idx_autonomous_system_number ON asn(autonomous_system_number);
+                    ''')
+
 
 def main():
-    if os.environ.get("IMPORT_TYPE", "TEST_LIST"):
-        import_test_lists()
-    else:
+    if os.environ.get("IMPORT_TYPE", "TEST_LIST") == "ASN":
         import_asn()
+    else:
+        import_test_lists()
 
 
 if __name__ == "__main__":
