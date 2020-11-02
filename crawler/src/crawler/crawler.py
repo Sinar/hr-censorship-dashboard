bd3cf83a5a95fe96d8d731648a5ca92efbfda566
Jeffrey04 <mycyberpet@yahoo.com>
2020-11-02 14:49:04 +0800

    add http status to logs

--- crawler/src/crawler/crawler.py
+++ crawler/src/crawler/crawler.py
@@ -80,10 +80,12 @@ async def run():
                 )
 
                 if not response.ok:
-                    logging.info(
-                        "%s: Error fetching measurements of %s, skipping",
+                    logging.error(
+                        "%s: Error fetching measurements of %s (%s %s), skipping",
                         os.environ.get("COUNTRY_CODE", "MY"),
                         row["url"],
+                        response.status,
+                        response.reason,
                     )
                     sleep()
                     continue
@@ -126,14 +128,15 @@ async def run():
         logging.info("%s: populating isp cache", os.environ.get("COUNTRY_CODE", "MY"))
         isp_cursor.execute(
             """
-            INSERT INTO isp(country_code, isp, asn)
-                SELECT      DISTINCT m.probe_cc,
-                            COALESCE(a.autonomous_system_organization, 'unknown'),
-                            REPLACE(m.probe_asn, 'AS', '')
-                FROM        summary_measurements m
-                LEFT JOIN   asn a
-                ON          REPLACE(a.autonomous_system_number, 'AS', '') = REPLACE(m.probe_asn, 'AS', '')
-                WHERE       LOWER(m.probe_cc) = %s
+            INSERT
+            INTO        isp(country_code, isp, asn)
+            SELECT      DISTINCT m.probe_cc,
+                        COALESCE(a.autonomous_system_organization, 'unknown'),
+                        REPLACE(m.probe_asn, 'AS', '')
+            FROM        summary_measurements m
+            LEFT JOIN   asn a
+            ON          REPLACE(a.autonomous_system_number, 'AS', '') = REPLACE(m.probe_asn, 'AS', '')
+            WHERE       LOWER(m.probe_cc) = %s
             """,
             (os.environ.get("COUNTRY_CODE", "MY").lower(),),
         )
@@ -147,42 +150,43 @@ async def run():
         )
         replace_cursor.execute(
             """
-            REPLACE INTO summary_view(year, country, category, count)
-                SELECT      m.year AS year,
-                            m.probe_cc AS country,
-                            COALESCE(sc.category_code, sg.category_code) AS category,
-                            COUNT(DISTINCT input) AS count
-                FROM        summary_measurements m
-                LEFT JOIN   (
-                                SELECT  url, category_code
-                                FROM    sites
-                                JOIN    (
-                                    SELECT     country_code, url, MAX(import_date) AS import_date
-                                    FROM       sites
-                                    GROUP BY   country_code, url
-                                ) AS sgmax
-                                USING   (country_code, url, import_date)
-                                WHERE   LOWER(country_code) = 'global'
-                            ) AS sg
-                ON          (m.input = sg.url)
-                LEFT JOIN   (
-                                SELECT  url, category_code
-                                FROM    sites
-                                JOIN    (
-                                    SELECT     country_code, url, MAX(import_date) AS import_date
-                                    FROM       sites
-                                    GROUP BY   country_code, url
-                                ) AS scmax
-                                USING   (country_code, url, import_date)
-                                WHERE   LOWER(country_code) = %s
-                            ) AS sc
-                ON          (m.input = sc.url)
-                JOIN        isp i
-                ON          (LOWER(m.probe_cc) = LOWER(i.country_code) AND REPLACE(m.probe_asn, 'AS', '') = i.asn)
-                WHERE       LOWER(m.probe_cc) = %s
-                            AND m.year = %s
-                            AND i.isp != 'unknown'
-                GROUP BY    m.year, m.probe_cc, category
+            REPLACE
+            INTO        summary_view(year, country, category, count)
+            SELECT      m.year AS year,
+                        m.probe_cc AS country,
+                        COALESCE(sc.category_code, sg.category_code) AS category,
+                        COUNT(DISTINCT input) AS count
+            FROM        summary_measurements m
+            LEFT JOIN   (
+                            SELECT  url, category_code
+                            FROM    sites
+                            JOIN    (
+                                SELECT     country_code, url, MAX(import_date) AS import_date
+                                FROM       sites
+                                GROUP BY   country_code, url
+                            ) AS sgmax
+                            USING   (country_code, url, import_date)
+                            WHERE   LOWER(country_code) = 'global'
+                        ) AS sg
+            ON          (m.input = sg.url)
+            LEFT JOIN   (
+                            SELECT  url, category_code
+                            FROM    sites
+                            JOIN    (
+                                SELECT     country_code, url, MAX(import_date) AS import_date
+                                FROM       sites
+                                GROUP BY   country_code, url
+                            ) AS scmax
+                            USING   (country_code, url, import_date)
+                            WHERE   LOWER(country_code) = %s
+                        ) AS sc
+            ON          (m.input = sc.url)
+            JOIN        isp i
+            ON          (LOWER(m.probe_cc) = LOWER(i.country_code) AND REPLACE(m.probe_asn, 'AS', '') = i.asn)
+            WHERE       LOWER(m.probe_cc) = %s
+                        AND m.year = %s
+                        AND i.isp != 'unknown'
+            GROUP BY    m.year, m.probe_cc, category
             """,
             (
                 os.environ.get("COUNTRY_CODE", "MY").lower(),
